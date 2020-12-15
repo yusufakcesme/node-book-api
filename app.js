282d4f92b5ef168bd3a456dba17a5d0649182842
yusuf akcesme <yusufakcesme0@gmail.com>
2020-12-15 20:36:02 +0300

    authentication

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ const app = express();
 // db connection
 const db = require('./helpers/db')();
 
+// Config
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
 
+// Middleware
+const verifyToken = require('./middlewares/verify-token');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', verifyToken);
 app.use('/api/books', book);
 
 // catch 404 and forward to error handler
